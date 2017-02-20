e75b2ccc398cbc7df80ceb2afbd6703a45d31f61
Daniel Hood <hood.danielc@gmail.com>
2017-02-19 23:13:18 -0800

    Add nodejs app for manageing db

---
+++ server/db/install-scripts/create-users.sql
@@ -0,0 +1,7 @@
+CREATE USER admin_dev WITH PASSWORD '123123';
+CREATE USER admin_test WITH PASSWORD '123123';
+CREATE USER admin_prod WITH PASSWORD '123123';
+
+CREATE DATABASE hoc_dev OWNER admin_dev;
+CREATE DATABASE hoc_test OWNER admin_test;
+CREATE DATABASE hoc_prod OWNER admin_prod;
--- server/db/run.sh
+++ server/db/run.sh
@@ -1,3 +1,4 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker run -ti dhoodlum/havenofcode-db postgres -D /etc/postgresql/9.4/main
+docker run -ti\
+  dhoodlum/havenofcode-db
